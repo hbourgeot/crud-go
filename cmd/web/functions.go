3a820e8819c0b4629cb79cbd1dd933225c9a81bc
henrry <henrrybrgt@gmail.com>
2022-08-05 22:08:35 -0400

    added functions.go

---
+++ cmd/web/functions.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func a() {
+	fmt.Println("Coming soon...")
+}
+
+func b() {
+	fmt.Println("working on this project")
+}
+
+func c() {
+	fmt.Println("Go Crud... but, for what?")
+}
